286b802eb2464fddfa5881d1175934954911121a
Adam O'Neil <adamosoftware@gmail.com>
2023-11-24 09:15:48 -0500

    Add project files.

---
+++ NuGetPush.CLI/Options.cs
@@ -0,0 +1,18 @@
+﻿using CommandLine;
+
+namespace NuGetPush.CLI;
+
+internal class Options
+{
+	[Option('p', "ProjectPath", Default = ".")]
+	public string ProjectPath { get; set; } = default!;
+
+	[Option('k', "ApiKey")]
+	public string ApiKey { get; set; } = default!;
+
+	[Option('f', "FeedUrl")]
+	public string FeedUrl { get; set; } = "https://api.nuget.org/v3/index.json";
+
+	[Option('l', "LogPath")]
+	public string LogPath { get; set; } = "%localappdata%\\NuGetPush";
+}
---
+++ NuGetPush.CLI/Program.cs
@@ -0,0 +1,34 @@
+﻿using CommandLine;
+using NuGet.Versioning;
+using NuGetPush.CLI;
+using Serilog.Events;
+using Serilog;
+
+await Parser.Default.ParseArguments<Options>(args).WithParsedAsync(async o =>
+{
+	Log.Logger = new LoggerConfiguration()
+		.WriteTo.File(Path.Combine(o.LogPath, "NuGetPush.log"), LogEventLevel.Information, rollingInterval: RollingInterval.Month, retainedFileCountLimit: 3)
+		.CreateLogger();
+
+	var local = GetLocalPackageInfo(o.ProjectPath);
+	var onlineVersion = await GetOnlinePackageVersion(local.PackageId, o.FeedUrl);
+
+	if (local.Version > onlineVersion)
+	{
+
+	}
+
+	await Log.CloseAndFlushAsync();
+});
+
+
+
+static Task<NuGetVersion> GetOnlinePackageVersion(string packageId, string feedUrl)
+{
+	throw new NotImplementedException();
+}
+
+static (NuGetVersion Version, string PackageId, string LocalFile) GetLocalPackageInfo(string path)
+{	
+	throw new NotImplementedException();
+}
